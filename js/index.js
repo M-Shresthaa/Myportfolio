b4e107929f6f41768dc634066fd7d297b38acb15
Mshrestha <ms21304011@ga.ttc.ac.jp>
2022-11-05 16:40:22 +0900

    Added info in coffee sites and fewer changes done.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+alert("The site is in beta mode (under construction)")
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -10,3 +12,24 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
